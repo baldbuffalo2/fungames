e926c23b623c23546b4b2bf0bd714241f1093c99
baldbuffalo2 <111616558+baldbuffalo2@users.noreply.github.com>
2022-11-19 15:11:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const btn = document.getElementById('btn');
-
-// ✅ Change button text on click
-btn.addEventListener('click', function handleClick() {
-  btn.textContent = 'Button clicked';
-});
-
-/**
- * ✅ If you need to change the button's inner HTML use:
- *  - `innerHTML` instead of `textContent`
- */
+function func() {
+        document.getElementById('accountDetails').value  = 'server';
+    }
